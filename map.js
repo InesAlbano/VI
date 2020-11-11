fb6eb4c2da3e37b9d53cb127788fa650f9af3af2
goncalo.barbas <goncalo_barbas98@hotmail.com>
2020-11-11 18:40:55 +0000

    beggining of map

---
+++ map.js
@@ -0,0 +1,50 @@
+//var available_countries = ["AT", "BE", "BG", "CY", "CZ", "DE", "DK", "EE", "ES", "FI", "FR", "GB", "GR", "HR", "HU", "IE", "IT", "LT", "LU", "LV", "MT", "NL", "NO", "PL", "PT", "RO", "SE", "SI", "SK", "TR"]
+//var selected_countries = [];
+
+//var margin = { top: 50, right: 40, bottom: 40, left: 60 };
+
+//https://www.d3-graph-gallery.com/graph/choropleth_basic.html
+
+// The svg
+var svg = d3.select("svg"),
+  width = +svg.attr("width"),
+  height = +svg.attr("height");
+
+// Map and projection
+var path = d3.geoPath();
+var projection = d3.geoMercator()
+  .scale(570)
+  .center([0,20])
+  .translate([width / 2, height / 2]);
+
+// Data and color scale
+var data = d3.map();
+var colorScale = d3.scaleThreshold()
+  .domain([100000, 1000000, 10000000, 30000000, 100000000, 500000000])
+  .range(d3.schemeBlues[7]);
+
+// Load external data and boot
+d3.queue()
+  .defer(d3.json, "map.json")
+  .defer(d3.csv, "population.csv", function(d) { 
+    data.set(d.code, +d.pop);
+  })
+  .await(ready);
+
+function ready(error, topo) {
+  // Draw the map
+  svg.append("g")
+    .selectAll("path")
+    .data(topo.features)
+    .enter()
+    .append("path")
+      // draw each country
+      .attr("d", d3.geoPath()
+        .projection(projection)
+      )
+      // set the color of each country
+      .attr("fill", function (d) {
+        d.total = data.get(d.id) || 0;
+        return colorScale(d.total);
+      });
+    }
