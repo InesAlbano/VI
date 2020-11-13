30eae7381fe03882e170922d6647a7315db8bc5d
Inês Albano <ines.albano@tecnico.ulisboa.pt>
2020-11-13 19:28:21 +0000

    starting to work

--- map.js
+++ map.js
@@ -5,8 +5,30 @@
 
 //https://www.d3-graph-gallery.com/graph/choropleth_basic.html
 
+var w = 1250;
+var h = 720;
+var projection = d3.geoMercator().center([-55, 55]).scale([w * 1.8 / (1.2 * Math.PI)]).translate([(w / 72) - 100, h / 2]);
+
+var countriesGroup;
+var countries;
+
+var path = d3.geoPath().projection(projection);
+
+var colorScale = d3.scaleThreshold().domain([0, 2, 4, 6, 8, 10]).range(d3.schemeBlues[7]);
+
+var svg;
+var labels;
+var legend;
+
+var limit_selection = 0;
+var limit_flag = 0;
+var limit_colors = d3.scaleOrdinal().range(["#F40C05", "#FBE52B", "#fb9224"]);
+var limit_colors_used = [];
+
+var line_counter = -1;
+
 // The svg
-var svg = d3.select("svg"),
+ svg = d3.select("svg"),
   width = +svg.attr("width"),
   height = +svg.attr("height");
 
@@ -25,46 +47,50 @@ var colorScale = d3.scaleThreshold()
 
 // Load external data and boot
 d3.queue()
-  .defer(d3.json, "map.json")
-  /*.defer(d3.csv, "population.csv", function(d) { 
-    data.set(d.code, +d.pop);*/
-  /*/.defer(d3.csv, "csv/Q1.csv", function(d) { 
-    for (var i = 0; i < d3.keys(d).length; i++) {  //[Year, Country, code, Education, AVG] for Q1
-      if(d3.values(d)[3] === '0-2'){
-        if(d3.values(d)[0] === '2005'){
-          data.set(d3.values(d)[2],+d3.values(d)[4]);
-        }
-      }
-    }*/
-    
-    
-  .defer(d3.json, "csv/Q1.json", function(d) { 
-    console.log('AAAA');
-    for (var i in d){
-      if(d[i].ISCED11 === '0-2'){
-        if(d[i].Year == '2005'){        
-          data.set(d[i]['Country'],+d[i]['AVG']);
-        }
-      }
-    }
-  }).await(ready);
+    .defer(d3.json, 'map.json')
+    .defer(d3.json, 'csv/Q1.json')
+    .await(ready);
 
-function ready(error, topo) {
-  console.log("BBBB");
-  // Draw the map
-  svg.append("g")
-    .selectAll("path")
-    .data(topo.features)
-    .enter()
-    .append("path")
-      // draw each country
-      .attr("d", d3.geoPath()
-        .projection(projection)
-      )
-      // set the color of each country
-      .attr("fill", function (d) {
-        d.total = data.get(d.id) || 0;
-        console.log(topo.features);
-        return colorScale(d.total);
+
+function ready(error, data, c) { // c is the csv
+    
+  
+  
+  const c1 = {};
+  c.forEach(d => { 
+    if (d.Year == "2005")
+      if (d.ISCED11 === '0-2'){
+          c1[d.code] = d.AVG;
+        }
       });
-    }
+  data.features.forEach(d => { 
+    d.AVG = c1[d.id];
+    console.log(d.id + ' '+d.AVG); // correct NL in Q.json doesnt exist
+  });
+  //console.log(c1)
+
+  countriesGroup = svg.append("g").attr("id", "map");
+  countries = countriesGroup
+      .selectAll("path")
+      .data(data.features)
+      .enter()
+      .append("path")
+      .attr("d", path)
+      .attr("fill", function (d) {
+        //console.log('AAA');
+          var value;
+          for (var i = 0; i < 27; i++) {
+            if (d.id === c[i].code){
+              value = c[i].AVG;
+              console.log('CCC');
+            }
+          }
+          return colorScale(value);
+      })
+      .attr("id", function (d) {
+          return d.id;
+      })
+      .attr("class", "country")
+      .style('stroke', '#515151')
+      .style('stroke-width', 1)
+}
