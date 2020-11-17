2be896b0542cd8814d9473036dc009acb12e0b45
Inês Albano <ines.albano@tecnico.ulisboa.pt>
2020-11-17 00:11:49 +0000

    passing variables through localstorage

--- map.js
+++ map.js
@@ -1,10 +1,17 @@
-/* simple map: https://bl.ocks.org/d3noob/f052595e2f92c0da677c67d5cf6f98a1 */
+var c = localStorage.getItem("countries");
+var e = localStorage.getItem("education");
+var y = localStorage.getItem("years");
+var v = localStorage.getItem("variable");
+console.log(c);
+console.log(e);
+console.log(y);
+console.log(v);
+//TODO these are all strings, if list, needs to be splitted
 
 var width = 960,
     height = 500;
 
-//var color = d3.scaleQuantize([1, 10], d3.schemeBlues[9]);
-
+//TODO color needs to change 
 var colorScale = d3.scaleThreshold()
   .domain([5, 10, 20, 30, 40, 50])
   .range(d3.schemeBlues[7]);
@@ -23,9 +30,7 @@ var path = d3.geoPath()
 
 var g = svg.append("g");
 
-// load and display the World
 d3.json("csv/map.json").then(function(data) {
-
   d3.json("csv/Q1.json").then(function(data2) {
     var counter = 0;
     g.selectAll("path")
@@ -36,9 +41,10 @@ d3.json("csv/map.json").then(function(data) {
       .style("fill", function(d) {
         var val;
         data2.forEach(d2 => {
-          if (d2.Year == '2005'){
-            if (d2.ISCED11 === '0-2'){
-              if (d2.code === d.id) { // TODO need to correct NL because dataset of Q1 does not contain id NL
+          if (d2.Year == y){
+            if (d2.ISCED11 === e){
+              if (d2.code === d.id) { 
+                // TODO need to correct NL because dataset of Q1 does not contain id NL
                 val = d2.AVG;
               }
             }
