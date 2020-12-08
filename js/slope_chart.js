f95b6510197ef9da82af17e750dd0cae28af195c
Inês Albano <ines.albano@tecnico.ulisboa.pt>
2020-12-08 21:08:18 +0000

    line corrected + avg of years in slope chart ( used line chart=)

--- js/slope_chart.js
+++ js/slope_chart.js
@@ -25,9 +25,9 @@ function updateLine(){
 }
 
 function analyzer(inequality, education) {
-	switch (inequality) {
+	/*switch (inequality) {
     case "GDP": // no education
-      d3.select("#line-svg").remove();
+      d3.select("#slope-svg").remove();
       d3.json("csv/LineChart/gdp.json").then(function (data) {
         dataset = data;
 
@@ -62,11 +62,11 @@ function analyzer(inequality, education) {
           }
           countries_filtered_years.push(aux);
         }
-      line_chart(countries_filtered_years, maximo, minimo, inequality);
+      slope_chart(countries_filtered_years, maximo, minimo, inequality);
       });
       break
     case "Employment":
-      d3.select("#line-svg").remove();
+      d3.select("#slope-svg").remove();
       d3.json("csv/LineChart/Q2_total.json").then(function (data) { //parse data
         dataset = data;
 
@@ -103,11 +103,11 @@ function analyzer(inequality, education) {
           }
           countries_filtered_years.push(aux);
         }
-      line_chart(countries_filtered_years, maximo, minimo, inequality);
+      slope_chart(countries_filtered_years, maximo, minimo, inequality);
       });       
       break
     case "Income":
-      d3.select("#line-svg").remove();
+      d3.select("#slope-svg").remove();
       d3.json("csv/LineChart/Q4.json").then(function (data) {
         dataset = data;
 
@@ -147,11 +147,11 @@ function analyzer(inequality, education) {
         }
         countries_filtered_years.push(aux);
         }
-      line_chart(countries_filtered_years,maximo,minimo, inequality);
+      slope_chart(countries_filtered_years,maximo,minimo, inequality);
       });          
       break
     case "Education":
-      d3.select("#line-svg").remove();
+      d3.select("#slope-svg").remove();
       d3.json("csv/LineChart/Q3_total.json").then(function (data) { //parse data
         dataset = data;
 
@@ -188,11 +188,11 @@ function analyzer(inequality, education) {
           }
           countries_filtered_years.push(aux);
         }
-      line_chart(countries_filtered_years, maximo, minimo, inequality);
+      slope_chart(countries_filtered_years, maximo, minimo, inequality);
       });       
       break
       case "Women-high-pos": // no education
-      d3.select("#line-svg").remove();
+      d3.select("#slope-svg").remove();
       d3.json("csv/LineChart/Q6.json").then(function (data) {
         dataset = data;
 
@@ -229,11 +229,11 @@ function analyzer(inequality, education) {
           }
           countries_filtered_years.push(aux);
         }
-      line_chart(countries_filtered_years, maximo, minimo, inequality);
+      slope_chart(countries_filtered_years, maximo, minimo, inequality);
       });
       break
     case "Poverty":
-      d3.select("#line-svg").remove();
+      d3.select("#slope-svg").remove();
       d3.json("csv/LineChart/Q1.json").then(function (data) {
         dataset = data;
 
@@ -270,11 +270,11 @@ function analyzer(inequality, education) {
           } 
           countries_filtered_years.push(aux);
         }
-      line_chart(countries_filtered_years,maximo,minimo, inequality);
+      slope_chart(countries_filtered_years,maximo,minimo, inequality);
       });
       break
     case "GWG": 
-    d3.select("#line-svg").remove();
+    d3.select("#slope-svg").remove();
     d3.json("csv/LineChart/Q4_b.json").then(function (data) {
       dataset = data;
 
@@ -308,15 +308,15 @@ function analyzer(inequality, education) {
         }
         if(minimo>0){
           minimo = 0;
-        } else {
+        } else { 
           minimo = minimo * 2;
         }
         countries_filtered_years.push(aux);
       }
-      line_chart(countries_filtered_years, maximo, minimo, inequality);
+      slope_chart(countries_filtered_years, maximo, minimo, inequality);
       });
       break
-    default:
+    default:*/
 
       d3.json("csv/LineChart/gdp.json").then(function (data) {
         dataset = data;
@@ -337,26 +337,33 @@ function analyzer(inequality, education) {
         var countries_filtered_years=[];
 
         for(let i=0; i<selected_countries.length; i++) {
+          gdp = 0;
           var aux=[];
           for(let j=0; j<selected_countries[i].length; j++){
             if(yearsv2.includes(selected_countries[i][j].Year)) {
-              aux.push(selected_countries[i][j]);
+              gdp = gdp + selected_countries[i][j].GDP;
               if(maximo<selected_countries[i][j].GDP){
                 maximo=selected_countries[i][j].GDP;
               }
               if(minimo>selected_countries[i][j].GDP){
                 minimo=selected_countries[i][j].GDP;
               }
+
             }
           }
+          dic = {}
+            dic['Country'] = selected_countries[i][0].Country;
+            dic['Year'] = 2010;
+            dic['GDP'] = gdp/yearsv2.length;
+            aux.push(dic);
           if(minimo>0){
             minimo=0;
           }
           countries_filtered_years.push(aux);
         }
-      line_chart(countries_filtered_years, maximo, minimo, "GDP");
+      slope_chart(countries_filtered_years, maximo, minimo, "GDP");
       });
-	}
+	//}
 }
 
 var div = d3.select("body").append("div")	
@@ -365,7 +372,7 @@ var div = d3.select("body").append("div")
 
 /* ------------------------------------- LINE CHART --------------------------------------- */
 
-function line_chart(paises, maximo,minimo, v) {
+function slope_chart(paises, maximo,minimo, v) {
   // INITIAL VARS ____________________________________________________________________________
 
   var xscaleData = paises[0].map((a) => a.Year);
@@ -374,18 +381,19 @@ function line_chart(paises, maximo,minimo, v) {
   var xscale = d3
     .scalePoint()
     .domain(xscaleData)
-    .range([padding, width - padding]);
+    //.range([padding, width - padding]);
+    .range([padding, padding]);
 
   var hscale = d3
     .scaleLinear()
     .domain([minimo, maximo])
     .range([height - padding, padding]);
 
-  // Define image SVG; everything of SVG will be append on the div of line_chart
+  // Define image SVG; everything of SVG will be append on the div of slope_chart
   var svg = d3
-    .select("#line_chart")
-    .append("svg") // appends an svg to the div 'line_chart'
-    .attr("id", "line-svg")
+    .select("#slope_chart")
+    .append("svg") // appends an svg to the div 'slope_chart'
+    .attr("id", "slope-svg")
     .attr("width", width)
     .attr("height", height);
 
@@ -434,7 +442,8 @@ function line_chart(paises, maximo,minimo, v) {
           if (v === "GWG")
             return parseFloat(d.GenderWageGap.replace(",", "."));
         })
-        .attr("cx", function (d, i) {
+        .attr("cx", function (d) {
+          //return 0  
           return xscale(d.Year);
         })
         .attr("cy", function (d) {
@@ -456,7 +465,7 @@ function line_chart(paises, maximo,minimo, v) {
         })
         .on("click", function (){
           this.parentNode.appendChild(this);
-          var b = document.getElementById("line-svg").getElementsByClassName("line")
+          var b = document.getElementById("slope-svg").getElementsByClassName("line")
           for (let i = 0; i < b.length; ++i){
             if(this.attributes.name.value != b[i].attributes.id.value.replace('-Lines', '')){
               b[i].attributes.selected.value = false;
@@ -553,49 +562,6 @@ function line_chart(paises, maximo,minimo, v) {
         .datum(paises[i])
         .attr("fill", "none")
         .attr("stroke", "red")
-/*        function(d) {
-          var value = 0
-          var keys;
-
-          if (v === "GDP") {
-            keys = [90000, 150000, 500000, 750000, 1000000, 2500000]  
-            var colorScale = d3.scaleThreshold() 
-            .domain(keys)
-            .range(d3.schemeBlues[7]);  
-
-            d.forEach(function (d2) {
-              console.log(d, d2)
-              if ((years1.includes(d2.Year))) {   
-                value = value + parseFloat(d2.GDP);         
-              }
-              if ((years1.includes(d2.Year))) { 
-                localStorage.setItem(d2.Country, (value/years1.length).toFixed(1));
-              }               
-            })
-            return (value/years1.length) ? colorScale(value/years1.length) : "#1A1C1F";
-          }
-          else if (v === "Employment"){
-            value = value + parseFloat(d.AverageEmployment.replace(",", ".")); 
-            keys = [15, 30, 45, 60, 75, 100]
-          } 
-          else if (v === "Income"){
-            value = value + d.MoneyM+d.MoneyF;
-            keys = [10000, 20000, 30000, 40000, 50000, 60000]
-          } 
-          else if (v === "Education"){
-            value = value + parseFloat(d.AveragePercentage.replace(",", ".")); 
-          }
-          if (v === "Women-high-pos"){
-            value = value + d.growthRateWHP;
-          }
-          else if (v === "Poverty") {
-            value = value + d.AVG;
-          }
-
-          else if (v === "GWG"){ 
-            value = value + parseFloat(d.GenderWageGap.replace(",", "."));
-          }    
-        })*/
         .attr("stroke-width", 4)
         .attr("id", function(d){
           return d[0].Country +'-Lines';
@@ -666,49 +632,4 @@ function line_chart(paises, maximo,minimo, v) {
 
   var xaxis = d3
     .axisBottom() // we are creating a d3 axis
-    .scale(xscale) // we are adding our padding
-    //.tickValues(xscaleDataFiltered)
-    .tickSizeOuter(0);
-
-  svg
-    .append("g") // we are creating a 'g' element to match our x axis
-    .attr("transform", "translate(0," + (height - padding) + ")")
-    .attr("class", "xaxis") // we are giving it a css style
-    .call(xaxis);
-
-  // text label for the x axis
-  svg
-    .append("text")
-    .attr(
-      "transform",
-      "translate(" + width / 2 + " ," + (height - padding / 3) + ")"
-    )
-    .attr("class", "label")
-    .text("Year");
-}
-
-
-/* interaction */
-function changeLine(Country){
-  resetLines()
-  var a = document.getElementById(Country+'-Lines')
-  a.attributes.stroke.value = "yellow"
-  a.attributes.selected.value = "true"
-}
-
-function resetLines(){
-  var a = document.getElementById("line-svg").getElementsByClassName("plot");
-  for (let i = 0; i < a.length; ++i){
-    a[i].attributes.is_clicked.value = false;
-    a[i].attributes.stroke.value = "red";
-    a[i].attributes.fill.value = "red";
-    a[i].attributes.r.value = radius;
-  }
-
-  console.log("im here", document.getElementById("line-svg").getElementsByClassName("plot"))
-  var b = document.getElementById("line-svg").getElementsByClassName("line")
-  for (let i = 0; i < b.length; ++i){
-    b[i].attributes.selected.value = false;
-    b[i].attributes.stroke.value = "red"
-  }
 }
